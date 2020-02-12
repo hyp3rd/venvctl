28999071246ceb3d6a067fb463353080a427cd35
pantsel <tselentispanagis@gmail.com>
2020-02-12 23:39:56 +0100

    Implemented CLI

--- module/__main__.py
+++ module/__main__.py
@@ -1,11 +1,23 @@
 """DO NOT REMOVE THIS."""
 
-# import sys
-import os
 from pathlib import Path
+from typing import Optional
+import click
 from .main.venvctl import VenvCtl
 
+@click.command()
+@click.option('--config', help='The path to the virtual environments configuration file')
+@click.option('--out', required=False, help='The virtual environments output folder')
+def run(config: str, out: Optional[str] = None) -> None:
+    """
+    A program that generates virtual environments
+    and reports based on a predefined configuration.
+    """
+    config_file = Path(config)
+    output_dir = Path(out) if out else None
+    VenvCtl(config_file=config_file,
+            output_dir=output_dir).run()
+
+
 if __name__ == '__main__':
-    config_file: Path = Path(f'{os.getcwd()}/module/tests/config/venvs.json')
-    VenvCtl(
-        config_file=config_file).run()
+    run() # pylint: disable=no-value-for-parameter
