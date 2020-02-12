bfd3968bf02ad995db5e2df0d421d3d96f9bf864
Francesco Cosentino <fc@hyperd.sh>
2020-02-12 02:20:36 +0200

    REF

--- module/main/venvctl.py
+++ module/main/venvctl.py
@@ -1,14 +1,16 @@
-"""
-THIS SOFTWARE IS PROVIDED AS IS
-and under GNU General Public License. <https://www.gnu.org/licenses/gpl-3.0.en.html>
+"""THIS SOFTWARE IS PROVIDED AS IS.
+
+Released under GNU General Public License:
+<https://www.gnu.org/licenses/gpl-3.0.en.html>
+
 USE IT AT YOUR OWN RISK.
 
-VenvCtl is a simple python object to leverage virtual environments programmatically.
-VenvCtl is a wrapper around **virtualenv**.
+VenvCtl is a python object to leverage virtual environments programmatically.
+VenvCtl is a wrapper around virtualenv.
 
-The module is published on PyPi <https://pypi.org/project/venvctl/>.
-
-The code is available on GitLab <https://gitlab.com/hyperd/venvctl>.
+Tools is a helper object.
+This module is part of VenvCtl: <https://pypi.org/project/venvctl/>.
+The code is available on GitLab: <https://gitlab.com/hyperd/venvctl>.
 """
 
 
@@ -18,7 +20,7 @@ import json
 import subprocess
 from pathlib import Path
 from typing import Any, List, Tuple, Dict
-import virtualenv
+import shutil
 from piphyperd import PipHyperd
 
 PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
@@ -36,28 +38,34 @@ class VenvCtl:
     python_path -- Path to the python binary to use
     """
 
-    def __init__(self, config_file: Path):
+    def __init__(self, config_file: Path,
+                 python_binary: Path = Path("/usr/local/bin/python3.8")):
+        """Init the venvctl providing a venvs {config_path}."""
         # venvs config file
         self.config_file: Path = Path(config_file)
         # venvs base dir
         self.venvs_path = Path(f'{os.getcwd()}/python-venvs')
         # path of the base venv
         self.base_venv_path = Path(f'{self.venvs_path}/base')
+        # path to the python binary to use
+        self.python_binary = python_binary
 
     @property
     def __get_bash_activation_fix(self) -> str:
-        """
-        venv activation fix for bash
+        """FIX the bashvenv activation. WARNING.
 
-        WARNING:
-        If you have any bash profile customization at the `cd` command,
-        this fix will breake.
+        If you have any bash profile customization
+        at the `cd` command, this fix will breake.
         """
+        return "VIRTUAL_ENV=$(cd $(dirname '$BASH_SOURCE'); dirname `pwd`)"
 
-        return 'VIRTUAL_ENV=$(cd $(dirname "$BASH_SOURCE"); dirname `pwd`)'
+    @property
+    def __get_venv_cmd(self) -> str:
+        """Return virtualenv command."""
+        return 'virtualenv --activators bash --copies'
 
     def __get_config(self):
-        """ get the venvs config file """
+        """Get the venvs config file."""
         with open(self.config_file, 'r') as file:
             config = json.load(file)
 
@@ -65,7 +73,7 @@ class VenvCtl:
 
     @staticmethod
     def __parse_venvs(config) -> Tuple[Any, Any, Any]:
-        """ helper method to parse the venvs config file """
+        """Parse the venvs config file."""
         base_venv = config["base"]
 
         all_venvs = config["venvs"]
@@ -80,44 +88,46 @@ class VenvCtl:
         return base_venv, regulars_venvs, networking_venvs
 
     @staticmethod
-    def install_packages(venv_path: Path, venv_packages: List[str]) -> Tuple[str, str, int]:
-        """ Install packages within a specific virtual environment """
+    def install_packages(venv_path: Path,
+                         venv_packages: List[str]) -> Tuple[str, str, int]:
+        """Install packages within a specific virtual environment."""
         subprocess.call('source {}/bin/activate'.format(venv_path), shell=True)
-
         piphyperd = PipHyperd(python_path=Path(f'{venv_path}/bin/python3'))
 
         install_report, install_errors, install_exitcode = piphyperd.install(
             *venv_packages)
 
-        virtualenv.make_environment_relocatable(venv_path)
-
+        # virtualenv.make_environment_relocatable(venv_path)
         return install_report, install_errors, install_exitcode
 
     def __create_base_venv(self, venv_packages: List[str]):
-        """ create a virtual environment with the shared packages """
-
-        virtualenv.create_environment(
-            home_dir=self.base_venv_path, symlink=False)
+        """Create a virtual environment with the shared packages."""
+        subprocess.call(
+            f'{self.python_binary} -m {self.__get_venv_cmd} {self.base_venv_path}',
+            shell=True)
+        # virtualenv.create_environment(
+        #     home_dir=self.base_venv_path, symlink=False)
 
         self.install_packages(self.base_venv_path, venv_packages)
 
-        with open('{}/bin/activate'.format(self.base_venv_path), 'r') as activate_file:
+        with open(f'{self.base_venv_path}/bin/activate', 'r') as activate_file:
             content = activate_file.read()
 
-        content = content.replace('VIRTUAL_ENV="{}"'.format(
-            self.base_venv_path), self.__get_bash_activation_fix)
+        content = content.replace(f"VIRTUAL_ENV='{self.base_venv_path}'",
+                                  self.__get_bash_activation_fix)
 
-        with open('{}/bin/activate'.format(self.base_venv_path), 'w') as activate_file:
+        with open(f'{self.base_venv_path}/bin/activate', 'w') as activate_file:
             activate_file.write(content)
 
-    def __create_from_base(self, venv_path: Path, venv_packages: List[str]) -> Tuple[str, str, int]:
-
-        virtualenv.copy_file_or_folder(self.base_venv_path, venv_path)
+    def __create_from_base(self, venv_path: Path,
+                           venv_packages: List[str]) -> Tuple[str, str, int]:
+        shutil.copytree(src=self.base_venv_path, dst=venv_path)
+        # virtualenv.copy_file_or_folder(self.base_venv_path, venv_path)
         return self.install_packages(venv_path, venv_packages)
 
     @staticmethod
     def venv_audit(venv_path: Path):
-        """ Runs audit against a specific virtual environment """
+        """Run audit against a specific virtual environment."""
         piphyperd = PipHyperd(python_path=Path(f'{venv_path}/bin/python3'))
 
         pip_freeze_report, _, _ = piphyperd.freeze()
@@ -131,6 +141,7 @@ class VenvCtl:
         for venv in venvs:
             install_report, install_errors, install_exitcode = self.__create_from_base(
                 f'{self.venvs_path}/{venv["name"]}', venv["packages"])
+
             pip_freeze_report, pip_check_report, pip_outdated_report = self.venv_audit(
                 f'{self.venvs_path}/{venv["name"]}')
 
@@ -147,10 +158,11 @@ class VenvCtl:
             }
 
             reports.Reports().generate_reports(
-                f'{self.venvs_path}/reports', venv["name"], reports_map, install_exitcode)
+                f'{self.venvs_path}/reports',
+                venv["name"], reports_map, install_exitcode)
 
     def run(self):
-        """ run the virtual environments generation """
+        """Run the virtual environments generation."""
         config = self.__get_config()
         base_venv, regulars_venvs, networking_venvs = self.__parse_venvs(
             config)
--- module/utils/tools.py
+++ module/utils/tools.py
@@ -1,13 +1,13 @@
-"""
-THIS SOFTWARE IS PROVIDED AS IS
-and under GNU General Public License. <https://www.gnu.org/licenses/gpl-3.0.en.html>
+"""THIS SOFTWARE IS PROVIDED AS IS.
+
+Released under GNU General Public License:
+<https://www.gnu.org/licenses/gpl-3.0.en.html>
+
 USE IT AT YOUR OWN RISK.
 
-IO is a python object helper for IO operations.
-
-This module is part of VenvCtl, published on PyPi <https://pypi.org/project/venvctl/>.
-
-The code is available on GitLab <https://gitlab.com/hyperd/venvctl>.
+Tools is a helper object.
+This module is part of VenvCtl: <https://pypi.org/project/venvctl/>.
+The code is available on GitLab: <https://gitlab.com/hyperd/venvctl>.
 """
 
 import os
@@ -18,20 +18,20 @@ from pathlib import Path
 
 
 class Tools:
-    """
-    Tools and Utils.
-    """
+    """Tools and Utils."""
 
     @staticmethod
     def packer(venv_path: Path, venv_name: str) -> str:
-        """ generates a tarball of the specified virtual environment """
+        """Generate a tarball of the specified virtual environment."""
         builds_path = "{}/builds".format(venv_path)
         if not os.path.isdir(builds_path):
             os.mkdir(builds_path)
 
-        with tarfile.open("{}/{}.tar.gz".format(builds_path, venv_name), "w:gz") as tar:
-            tar.add("{}/{}".format(venv_path, venv_name), arcname=os.path.basename(
-                "{}/{}".format(venv_path, venv_name)), recursive=True)
+        with tarfile.open("{}/{}.tar.gz".format(builds_path, venv_name),
+                          "w:gz") as tar:
+            tar.add("{}/{}".format(venv_path, venv_name),
+                    arcname=os.path.basename(
+                        "{}/{}".format(venv_path, venv_name)), recursive=True)
 
             tarball_content = io.StringIO()
 
