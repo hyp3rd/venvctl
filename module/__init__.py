183fba4228481e93dffe364fcc79f5ef19e4a66b
Francesco Cosentino <fc@hyperd.sh>
2020-02-12 14:18:49 +0200

    REF

--- module/__init__.py
+++ module/__init__.py
@@ -1,13 +1,1 @@
-"""
-DO NOT REMOVE THIS
-"""
-
-import os
-import sys
-
-
-PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), './main'))
-if not PATH in sys.path:
-    sys.path.insert(1, PATH)
-    from .main.venvctl import VenvCtl
-del PATH
+"""DO NOT REMOVE THIS."""
