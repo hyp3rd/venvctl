39f53e143155e93a72f02a301c5dc0fc64d17ff4
Francesco Cosentino <fc@hyperd.sh>
2020-02-13 10:46:18 +0000

    Development

--- module/__main__.py
+++ module/__main__.py
@@ -5,13 +5,13 @@ from typing import Optional
 import click
 from .main.venvctl import VenvCtl
 
+
 @click.command()
 @click.option('--config', help='The path to the virtual environments configuration file')
 @click.option('--out', required=False, help='The virtual environments output folder')
 def run(config: str, out: Optional[str] = None) -> None:
-    """
-    A program that generates virtual environments
-    and corresponding reports, based on a predefined configuration.
+    """A program that generates virtual environments along with related
+    reports, based on a predefined configuration.
     """
     config_file = Path(config)
     output_dir = Path(out) if out else None
@@ -20,4 +20,4 @@ def run(config: str, out: Optional[str] = None) -> None:
 
 
 if __name__ == '__main__':
-    run() # pylint: disable=no-value-for-parameter
+    run()  # pylint: disable=no-value-for-parameter
