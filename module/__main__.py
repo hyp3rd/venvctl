c88ccf32e130c27fe231ae775e38a0f989d67f5d
Francesco Cosentino <fc@hyperd.sh>
2020-02-13 11:53:29 +0000

    Added --python option in CLI

--- module/__main__.py
+++ module/__main__.py
@@ -9,13 +9,18 @@ from .main.venvctl import VenvCtl
 @click.command()
 @click.option('--config', help='The path to the virtual environments configuration file')
 @click.option('--out', required=False, help='The virtual environments output folder')
-def run(config: str, out: Optional[str] = None) -> None:
-    """A program that generates virtual environments along with related
-    reports, based on a predefined configuration.
+@click.option('--python', required=False, help='The path to the python binary')
+def run(config: str,
+        out: Optional[str] = None,
+        python: Optional[str] = None) -> None:
+    """
+    A program that generates virtual environments
+    and corresponding reports, based on a predefined configuration.
     """
     config_file = Path(config)
     output_dir = Path(out) if out else None
     VenvCtl(config_file=config_file,
+            python_binary=python,
             output_dir=output_dir).run()
 
 
