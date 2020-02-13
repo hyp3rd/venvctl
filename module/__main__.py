d87b403518bb47032d77eb57e6d112f79eb35d3b
pantsel <tselentispanagis@gmail.com>
2020-02-13 16:18:49 +0100

    moved cli to a subpackage

--- module/__main__.py
+++ module/__main__.py
@@ -1,28 +1,6 @@
 """DO NOT REMOVE THIS."""
 
-from pathlib import Path
-from typing import Optional
-import click
-from .main.venvctl import VenvCtl
-
-
-@click.command()
-@click.option('--config', help='The path to the virtual environments configuration file')
-@click.option('--out', required=False, help='The virtual environments output folder')
-@click.option('--python', required=False, help='The path to the python binary')
-def run(config: str,
-        out: Optional[str] = None,
-        python: Optional[str] = None) -> None:
-    """
-    A program that generates virtual environments
-    and corresponding reports, based on a predefined configuration.
-    """
-    config_file = Path(config)
-    output_dir = Path(out) if out else None
-    VenvCtl(config_file=config_file,
-            python_binary=python,
-            output_dir=output_dir).run()
-
+from .cli.main import Cli
 
 if __name__ == '__main__':
-    run()  # pylint: disable=no-value-for-parameter
+    Cli().run()
