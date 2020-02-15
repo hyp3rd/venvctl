54b08ca288458ed2c0aed879022be800283bd1fa
pantsel <tselentispanagis@gmail.com>
2020-02-15 12:42:07 +0100

    Added configutils tests

---
+++ module/tests/configutils.py
@@ -0,0 +1,157 @@
+"""THIS SOFTWARE IS PROVIDED AS IS.
+
+Released under GNU General Public License:
+<https://www.gnu.org/licenses/gpl-3.0.en.html>
+
+USE IT AT YOUR OWN RISK.
+
+Configutils unit testing.
+
+This module is part of VenvCtl: <https://pypi.org/project/venvctl/>.
+The code is available on GitLab: <https://gitlab.com/hyperd/venvctl>.
+"""
+
+import unittest
+from ..utils import configutils
+
+
+class TestMethods(unittest.TestCase):
+    """Configutils unit testing class."""
+
+    def setUp(self) -> None:
+        """Test setup."""
+        self.valid_config = [{
+            "name": "base",
+            "packages": [
+                "docker==4.1.0",
+                "cryptography==2.7",
+                "whichcraft==Í0.5.2"
+            ]
+            }, {
+                "name": "ansible_2_6",
+                "parent": "base",
+                "packages": [
+                    "ansible==2.6",
+                    "identify==1.4.11"
+                ]
+            }, {
+                "name": "ansible_2_7",
+                "parent": "base",
+                "packages": [
+                    "ansible==2.7"
+                ]
+            }, {
+                "name": "ansible_2_9",
+                "parent": "base",
+                "packages": [
+                    "ansible==2.9"
+                ]
+            }, {
+                "name": "ansible_2_9_networking",
+                "parent": "ansible_2_9",
+                "packages": [
+                    "websocket-client==0.56.0",
+                    "urllib3==1.24.1",
+                    "tox==3.12.1"
+                ]
+            }]
+
+        self.invalid_config_item_parent = [{
+            "name": "base",
+            "packages": [
+                "docker==4.1.0",
+                "cryptography==2.7",
+                "whichcraft==0.5.2"
+            ]
+            }, {
+                "name": "ansible_2_6",
+                "parent": "whatever",
+                "packages": [
+                    "ansible==2.6",
+                    "identify==1.4.11"
+                ]
+            }, {
+                "name": "ansible_2_7",
+                "parent": "base",
+                "packages": [
+                    "ansible==2.7"
+                ]
+            }, {
+                "name": "ansible_2_9",
+                "parent": "base",
+                "packages": [
+                    "ansible==2.9"
+                ]
+            }, {
+                "name": "ansible_2_9_networking",
+                "parent": "ansible_2_9",
+                "packages": [
+                    "websocket-client==0.56.0",
+                    "urllib3==1.24.1",
+                    "tox==3.12.1"
+                ]
+            }]
+
+        self.invalid_config_type = {
+            "venvs": [
+                {
+                    "name": "test",
+                    "packages": []
+                }
+            ]}
+
+        self.invalid_config_item_prop = [
+                {
+                    "whatever": "test",
+                    "packages": []
+                }
+            ]
+
+        self.invalid_config_item_prop_type = [{
+            "name": False,
+            "packages": []}]
+
+        self.invalid_config_item_type = ["a string"]
+
+    def test_valid_config(self) -> None:
+        """Assert that the config is valid."""
+        self.assertTrue(configutils.validate_config(self.valid_config))
+
+    def test_invalid_config_type(self) -> None:
+        """Assert that the config type is valid."""
+        with self.assertRaises(AssertionError) as error:
+            configutils.validate_config(self.invalid_config_type)
+        self.assertEqual(configutils.ERRORS["INVALID_CONFIG_OBJECT_TYPE"], str(error.exception))
+
+    def test_invalid_config_item_prop(self) -> None:
+        """Assert that the config type is valid."""
+        with self.assertRaises(AssertionError) as error:
+            configutils.validate_config(self.invalid_config_item_prop)
+        self.assertEqual(
+            configutils.ERRORS["INVALID_ITEM_KEY"].replace("__KEY__", "whatever"),
+            str(error.exception))
+
+    def test_invalid_config_item_prop_type(self) -> None:
+        """Assert that the config type is valid."""
+        with self.assertRaises(AssertionError):
+            configutils.validate_config(self.invalid_config_item_prop_type)
+
+    def test_invalid_config_item_type(self) -> None:
+        """Assert that the config type is valid."""
+        with self.assertRaises(AssertionError) as error:
+            configutils.validate_config(self.invalid_config_item_type)
+        self.assertEqual(
+            configutils.ERRORS["INVALID_CONFIG_ITEM_TYPE"],
+            str(error.exception))
+
+    def test_invalid_config_item_parent(self) -> None:
+        """Assert that the config type is valid."""
+        with self.assertRaises(AssertionError) as error:
+            configutils.validate_config(self.invalid_config_item_parent)
+        self.assertEqual(
+            configutils.ERRORS["INVALID_ITEM_PARENT"].replace("__KEY__", "whatever"),
+            str(error.exception))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- module/utils/configutils.py
+++ module/utils/configutils.py
@@ -11,6 +11,16 @@ The code is available on GitLab: <https://gitlab.com/hyperd/venvctl>.
 
 from typing import Any, Dict
 
+ERRORS = {
+    "INVALID_CONFIG_OBJECT_TYPE": "The configuration object must be a list",
+    "INVALID_CONFIG_ITEM_TYPE": "The configuration items must be dicts",
+    "INVALID_ITEM_KEY": "Invalid configuration item key: \"__KEY__\"",
+    "INVALID_ITEM_KEY_TYPE": """
+    Invalid configuration item key type. \"__KEY__\" should be \"__TYPE__\"
+    """,
+    "INVALID_ITEM_PARENT": "Invalid configuration item parent: \"__KEY__\""
+}
+
 def valid_properties() -> Dict[Any, Any]:
     """Valid configuration item properties and types."""
     return {
@@ -27,25 +37,24 @@ def get_item_by_name(config: list, name: str) -> Any:
     """
     return next((item for item in config if item["name"] == name), None)
 
-def validate_config(config: Any) -> None:
+def validate_config(config: Any) -> bool:
     """Validate configuration file."""
 
     props = valid_properties()
 
     # Ensure that the config object is a list
-    assert isinstance(config, list), "The configuration object must be a list"
+    assert isinstance(config, list), ERRORS["INVALID_CONFIG_OBJECT_TYPE"]
 
     for item in config:
         # Ensure that config items are dictionaries
-        assert isinstance(item, dict), "The configuration items must be dicts"
+        assert isinstance(item, dict), ERRORS["INVALID_CONFIG_ITEM_TYPE"]
         for key, value in item.items():
             # Ensure config item key is valid
-            assert key in props, f'Invalid configuration item key: {key}'
-            assert isinstance(value, props[key]), f'''
-            Invalid configuration item key type. "{key}" should be {props[key]}'
-            '''
+            assert key in props, ERRORS["INVALID_ITEM_KEY"].replace("__KEY__", key)
+            assert isinstance(value, props[key]), ERRORS["INVALID_ITEM_PARENT"].replace("__KEY__", key).replace("__TYPE__", str(props[key]))
+            
             # Ensure that if defined, the parent exists
             if key == "parent":
-                assert get_item_by_name(config, value) is not None, f'''
-                Invalid configuration item parent: {value}
-                '''
+                assert get_item_by_name(config, value) is not None, ERRORS["INVALID_ITEM_PARENT"].replace("__KEY__", value)
+
+    return True
