f5a66443b4a381df465a5c9b58eddd48c547a2f3
Francesco Cosentino <fc@hyperd.sh>
2020-02-17 21:03:59 +0200

    version 0.3.3

--- module/main/release.py
+++ module/main/release.py
@@ -15,5 +15,5 @@ from __future__ import (absolute_import, division, print_function)
 
 __metaclass__ = type  # pylint: disable=invalid-name
 
-__version__ = '0.3.3.dev0'
+__version__ = '0.3.3.dev1'
 __author__ = 'Hyper(d)'
