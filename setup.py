096b1a05575cbe25566cf7a889bd635678cba4ad
Francesco Cosentino <fc@hyperd.sh>
2020-03-21 16:33:36 +0100

    removed compatibility with python 3.5

--- setup.py
+++ setup.py
@@ -105,7 +105,6 @@ setuptools.setup(
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
