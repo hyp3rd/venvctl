3271bfad23f1690d0ff7f1cc1bf0a78b920b9971
Francesco Cosentino <fc@hyperd.sh>
2020-03-22 22:14:18 +0100

    bumped piphyperd version to 1.9.4

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ setuptools.setup(
     ],
 
     install_requires=[
-        'piphyperd==1.9.3',
+        'piphyperd==1.9.4',
         'markd==0.1.20',
         'virtualenv==20.0.13',
         'click8==8.0.1',
