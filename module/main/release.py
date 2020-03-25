990eeece801ce6c417f58ab70bd3c764996977ff
Francesco Cosentino <fc@hyperd.sh>
2020-03-25 18:19:14 +0100

    renamed .python-version

--- module/main/release.py
+++ module/main/release.py
@@ -15,5 +15,5 @@ from __future__ import (absolute_import, division, print_function)
 
 __metaclass__ = type  # pylint: disable=invalid-name
 
-__version__ = "1.2.4"
+__version__ = "1.2.5"
 __author__ = "Hyper(d)"
