4fde088878159c90d74a59eaaac3aa5057bcdaf0
Francesco Cosentino <fc@hyperd.sh>
2020-04-19 20:01:35 +0200

    .gitignore improved

--- compl.bash
+++
@@ -1,9 +0,0 @@
-# venvctl bash completion start
-_venvctl_completion()
-{
-    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
-                   COMP_CWORD=$COMP_CWORD \
-                   VENVCTL_AUTO_COMPLETE=1 $1 2>/dev/null ) )
-}
-complete -o default -F _venvctl_completion venvctl
-# venvctl bash completion end
